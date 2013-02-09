e1ee10a1ad29f6cdb5fc84b38031fd9c060d8e78
reymond <ramonzizu51@hotmail.com>
2013-02-08 18:21:04 -0600

    progra finalizado pregunta tres calificaciones te da tu promedio

--- promedio.cpp
+++ promedio.cpp
@@ -9,6 +9,17 @@ int main (void){
 	scanf("%f",&cal2);
 	printf("\nescribe la tercera calificacion\n");
 	scanf("%f",&cal3);
+	promedio=(cal1+cal2+cal3)/3;
+	if (promedio>=6)
+	{
+		printf("tu promedio es %.1f",promedio);
+		printf("\nalumno aprobado");
+	}
+	else
+		{
+			printf("tu promedio es %.1f",promedio);
+			printf("alumno reprobado");
+		}
 	
 	
 }
