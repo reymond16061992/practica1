5fc24450e44c83734e91b8bc502de0743e1f9a64
reymond <ramonzizu51@hotmail.com>
2013-02-08 18:13:04 -0600

    principio de programa que pregunta tres calificaciones

---
+++ promedio.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (void){
+	float cal1,cal2,cal3,promedio;
+	printf("programa que permite obtener el promedio de tres calificaciones");
+	printf("\n");
+	printf("\nescribe la primera calificacion\n");
+	scanf("%f",&cal1);
+	printf("\nescribe la segunda calificacion\n");
+	scanf("%f",&cal2);
+	printf("\nescribe la tercera calificacion\n");
+	scanf("%f",&cal3);
+	
+	
+}
